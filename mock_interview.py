8c524a4ed44445b489fad6523814c5d8ddc95aba
Stephan Stylianides <s_stylianides@yahoo.com>
2017-07-21 14:45:03 -0700

    initial commit

---
+++ mock_interview.py
@@ -0,0 +1,77 @@
+'''
+Q1: What is the most influential book or blog post you’ve read regarding web development?
+
+Answer:
+
+The two most influential books that I've read regarding web development is "Learn Python
+the Hard Way" and "Learn More Python the Hard Way" by Zed Shaw.  Although not entirely about web
+development, I chose these two books because this is what gave me the foundation to dive deeper
+into web development and sharpen my Python skills. These two books definitely gave me the
+opportunity to get a solid understanding of programming in order to become a web developer.
+
+
+
+Q2: Tell me about a web application you have built. Why did you choose to build it? What did
+    you learn? What challenges did you face and how did you overcome them?
+
+Answer:
+
+
+
+Q3: Write a function in Python that takes a list of strings and returns a single string that
+    is an HTML unordered list (<ul>...</ul>) of those strings. You should include a brief
+    explanation of your code. Then, what would you have to consider if the original list was
+    provided by user input?
+
+Answer:
+'''
+CODE HERE
+
+'''
+Q4: List 2-3 attacks that web applications are vulnerable to. How do these attacks work?
+    How can we prevent those attacks?
+
+Answer:
+
+
+Q5: Below is some starter code for a Flask Web Application. Expand on that and include a
+    route that simulates rolling two dice and returns the result in JSON. You should include
+    a brief explanation of your code.
+
+from flask import Flask
+app = Flask(__name__)
+
+import json
+import random
+
+@app.route('/')
+def hello_world():
+ return 'Hello World!'
+
+if __name__ == '__main__':
+ app.debug = True
+ app.run()
+
+Answer:
+'''
+from flask import Flask
+app = Flask(__name__)
+
+import json
+import random
+
+@app.route('/')
+def hello_world():
+ return 'Hello World!'
+
+if __name__ == '__main__':
+ app.debug = True
+ app.run()
+
+'''
+Q6. If you were to start your full-stack developer position today, what would be your goals
+    a year from now?
+
+Answer:
+
+'''
